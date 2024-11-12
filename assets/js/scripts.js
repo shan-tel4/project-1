a5acf986929d081ea24601a191b61585e378fbf2
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2024-11-12 12:52:30 +0000

    new nav bar created

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,5 +3,5 @@ var navBar = document.getElementById("navBar");
             navBar.style.right = "0";
         }
         function hideMenu() {
-            navBar.style.right = "-200px";
+            navBar.style.right = "-250px";
         }
